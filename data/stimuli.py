8817a798d891581dfb7ed5d49032f9fefeb55ec2
KristijanArmeni <kristijan.armeni@gmail.com>
2021-01-13 20:48:31 -0500

    massive update of the word prediction task script. Included mlflow logging (as a test), added argparse and modified downstream code, fixed evaluation. stimuli.py: minor fix to add blank space between sentences.

--- data/stimuli.py
+++ data/stimuli.py
@@ -2,25 +2,31 @@ import random
 
 # define prefixes
 prefixes = {
-    "spl": "This is a list:",            # simple
-    "syn": "This is a mist:",            # styntactic
-    "exp": "Memorize this list:",        # explicit
-    "rnd": "Penguin was that were:",     # random
-    "cpl": "Before the meeting, Mary brainstormed the following list of words:"
+    "sce1": "Before the meeting, Mary brainstormed the following list of words:"
 }
 
 # define prompts
 prompts_word_prediction = {
-    "cnt": "The list contains:",
-    "str": "This list started with:",
-    "rnd": "At it goals with was:",
-    "cpl_first": "The first item on her word list was the following:",
-    "cpl_any": "One of the words on her list was the following:"
+    "sce1-1": "After the meeting, Mary read the list again:",
+
+    "sce1-3": " ".join(["After the meeting, Mary went for a walk. It was a busy day and she had to relax.",
+                        "It was a warm sunny day and the flowers in the park were blooming.",
+                        "When she got back, she read the list again:"]),
+
+    "sce1-5": " ".join(["After the meeting, Mary went for a walk. It was a busy day and she had to relax.",
+                        "It was a warm sunny day and the flowers in the park were blooming.",
+                        "While she was walking, she listened to the wonderful bird songs.",
+                        "During the walk, Mary could not stop thinking about the meeting.",
+                        "She was thinking about the discussions she had with her coworkers.",
+                        "Luckily, she met her neighbours Joanna and Ryan. They talked a little bit and then ",
+                        "Joanna and Ryan invited her to come over for a cup of coffee.",
+                        "Mary said she had time over the weekend. After they said goodbye, Mary started walking",
+                        "towards home. When she got back, she read the list again:"])
 }
 
 # define prefixes
 prefixes_perplexity = {
-    "sce1": "Before the meeting, Mary composed the following list of words:"
+    "sce1": "Before the meeting, Mary composed the following list of words:",
 }
 # Define a meainigful probe
 a = "The landlady informed me that he had left the house shortly after eight o’clock" \
--- next_word_prediction.py
+++ next_word_prediction.py
@@ -7,15 +7,64 @@ next_word_prediction.py "model_class" "config"
 
 """
 from experiment import Sampler
+import sys, os
+sys.path.append(os.path.join(os.getcwd(), 'data'))  # this is needed for stimuli module to be found
 from stimuli import prefixes, prompts_word_prediction
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import json
 import pandas as pd
+import torch
+import mlflow
+import argparse
+import datetime
 
 text_generation = True
 
+# collect input arguments
+parser = argparse.ArgumentParser(description="next_word_prediction.py")
+
+parser.add_argument("--do_sample", type=int, help="")
+parser.add_argument("--num_beams", type=int, help="")
+parser.add_argument("--temp", type=float, help="")
+parser.add_argument("--top_p", type=float, help="")
+parser.add_argument("--top_k", type=int, help="")
+parser.add_argument("--max_tokens", type=int, help="")
+parser.add_argument("--min_tokens", type=int, help="")
+parser.add_argument("--input_list", type=str, help="")
+parser.add_argument("--savename", type=str, help="")
+
+argins = parser.parse_args()
+argins.do_sample = bool(argins.do_sample)
+
+# if no sampling method is chosen, make the params None
+if not argins.do_sample:
+    argins.top_k = 0
+    argins.top_p = 1.0
+    argins.temp = 1.0
+
+print("do_sample={} setting the following params as:".format(argins.do_sample))
+print("top_p={}".format(argins.top_p))
+print("top_k={}".format(argins.top_k))
+print("num_beams={}".format(argins.num_beams))
+
+params = {
+    "do_sample": argins.do_sample,  # convert string to int, and int to bool
+    "num_beams": argins.num_beams,
+    "temperature": argins.temp,
+    "top_p": argins.top_p,
+    "top_k": argins.top_k,
+    "max_tokens": argins.max_tokens,   # the upper bound on the number of tokens to be generated
+    "min_tokens": argins.min_tokens  # the lower bound on the number of tokens to be generated
+}
+
 # load the word lists
-with open("./data/toronto.json") as f:
+fname = None
+if argins.input_list == "nonsemantic":
+    fname = "./data/toronto.json"
+elif argins.input_list == "semantic":
+    fname = "./data/semantic_lists.json"
+with open(fname) as f:
+    print("Loading {}".format(fname))
     stim = json.load(f)
 
 # convert list to strings
@@ -30,8 +79,8 @@ s = Sampler(model=model, tokenizer=tokenizer)
 
 # apply selection of conditions if needed
 prefixes_orig = prefixes.copy()
-prefixes = {key: prefixes_orig[key] for key in ["cpl"]}
-conditions = ["cpl_any", "cpl_first"]
+prefixes = {key: prefixes_orig[key] for key in ["sce1"]}
+conditions = ["sce1-1", "sce1-3", "sce1-5"]
 prompts_word_prediction_orig = prompts_word_prediction.copy()
 prompts_word_prediction = {key: prompts_word_prediction_orig[key] for key in conditions}
 
@@ -47,119 +96,132 @@ else:
 lst = []
 count = 1
 total = len(input_lists)*len(prefixes.keys())*len(prompts_word_prediction.keys())
-for k in num_tokens:
-    # loop over different prefixes:
-    for prefix_key in prefixes.keys():
-        for prompt in prompts_word_prediction.keys():
-            for l in input_lists:
 
-                print("Trial {}/{}".format(count, total))
+# set mlflow business
 
-                input_string = prefixes[prefix_key] + " " + \
-                               l + " " + \
-                               prompts_word_prediction[prompt]
-                print("Processing:\n{}".format(input_string))
+mlflow.set_experiment(experiment_name="next_word_prediction")
 
-                # convert string back to list, remove comas and periods
-                targets = l.replace(",", "").replace(".", "").split()
+with mlflow.start_run():
 
-                # execute this block for word prediction task
-                if not text_generation:
+    mlflow.set_tag("date-time", datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))
+    mlflow.log_params(params)
 
-                    logits = s.get_logits(input_string=input_string)
-                    # collect the next token prediction and its probability
-                    next_token_probs, next_token_ids = s.get_probs(logits=logits, k=50, p=0.9, n_tokens=k)
+    for k in num_tokens:
+        # loop over different prefixes:
+        for prefix_key in prefixes.keys():
+            for prompt in prompts_word_prediction.keys():
+                for l in input_lists:
 
-                    next_tokens = s.tokenizer.decode(next_token_ids[0])
+                    print("Trial {}/{}".format(count, total))
 
-                    print("Prediction at k == {}: {}".format(k, next_tokens))
+                    input_string = prefixes[prefix_key] + " " + \
+                                   l + " " + \
+                                   prompts_word_prediction[prompt]
 
-                    # score based on criteria
-                    ishit = None
-                    if ("starts with" in prompt) or ("the first item" in prompt) and k == 1:
-                        if k == 1:
-                            ishit = int(next_tokens == targets[0])
-                        elif k > 1:
+                    # convert string back to list, remove comas and periods
+                    targets = l.replace(",", "").replace(".", "").split()
+
+                    # execute this block for word prediction task
+                    if not text_generation:
+
+                        logits = s.get_logits(input_string=input_string)
+                        # collect the next token prediction and its probability
+                        next_token_probs, next_token_ids = s.get_probs(logits=logits, k=50, p=0.9, n_tokens=k)
+
+                        next_tokens = s.tokenizer.decode(next_token_ids[0])
+
+                        print("Prediction at k == {}: {}".format(k, next_tokens))
+
+                        # score based on criteria
+                        ishit = None
+                        if ("starts with" in prompt) or ("the first item" in prompt) and k == 1:
+                            if k == 1:
+                                ishit = int(next_tokens == targets[0])
+                            elif k > 1:
+                                ishit = any(item in next_tokens.split() for item in targets)
+                        else:  # otherwise count hit if the token is anywhere
                             ishit = any(item in next_tokens.split() for item in targets)
-                    else:  # otherwise count hit if the token is anywhere
-                        ishit = any(item in next_tokens.split() for item in targets)
 
-                    probs = None
-                    if k == 1:
-                        probs = str(next_token_probs[0][0].tolist())
-                    elif k > 1:
-                        probs = str(next_token_probs[0].tolist())
+                        probs = None
+                        if k == 1:
+                            probs = str(next_token_probs[0][0].tolist())
+                        elif k > 1:
+                            probs = str(next_token_probs[0].tolist())
 
-                    lst.append(
-                        {
-                            "pref": prefix_key,
-                            "prompt": prompt,
-                            "len": len(l),
-                            "prediction": next_tokens,
-                            "prob": probs,
-                            "ishit": ishit,
-                            "top-k": k
-                        }
-                    )
+                        lst.append(
+                            {
+                                "pref": prefix_key,
+                                "prompt": prompt,
+                                "len": len(l),
+                                "prediction": next_tokens,
+                                "prob": probs,
+                                "ishit": ishit,
+                                "top-k": k
+                            }
+                        )
 
-                # execute this block for text generation task
-                if text_generation:
+                    # execute this block for text generation task
+                    if text_generation:
 
-                    # set parameters for model.generate()
-                    num_beams = 4
-                    top_p = 0.95
-                    top_k = 60
+                        inputs = s.tokenizer.encode(input_string, add_special_tokens=False, return_tensors="pt")
 
-                    max_tokens = 15   # the upper bound on the number of tokens to be generated
-                    min_tokens = 5  # the lower bound on the number of tokens to be generated
+                        max_len = len(inputs[0]) + argins.max_tokens
 
-                    inputs = s.tokenizer.encode(input_string, add_special_tokens=False, return_tensors="pt")
+                        # set rng seed for reproducibility
+                        torch.manual_seed(54321)
 
-                    max_len = len(inputs[0]) + max_tokens
+                        outputs = s.model.generate(input_ids=inputs,
+                                                   max_length=max_len,
+                                                   min_length=len(inputs[0]) + argins.min_tokens,
+                                                   num_beams=argins.num_beams,
+                                                   temperature=argins.temp,                # setting this to 1 avoids beam search
+                                                   bad_words_ids=[[366], [1085]],          # block generation of '"' and ':'
+                                                   do_sample=argins.do_sample,
+                                                   top_k=argins.top_k,
+                                                   top_p=argins.top_p
+                                                   )
 
-                    outputs = s.model.generate(input_ids=inputs,
-                                               max_length=max_len,
-                                               min_length=len(inputs[0]) + min_tokens,
-                                               do_sample=True,              # don't do greedy search
-                                               num_beams=num_beams,         # setting this to 1 avoids beam search
-                                               bad_word_ids=[366, 1085],    # block generation of '"' and ':'
-                                               top_p=top_p,                 # restrict to tokens withing the p prob mass
-                                               top_k=top_k)                 # sample from top_k number of tokens
+                        # fill in the output list, go back -max_tokens
+                        out_str = s.tokenizer.decode(outputs[0][-argins.max_tokens::])
+                        # grab the string generated after ":", in case it was shorter than max_tokens
+                        out_str = out_str.split(":")[-1]
 
-                    # fill in the output list, go back -max_tokens
-                    out_str = s.tokenizer.decode(outputs[0][-max_tokens::])
-                    # grab the string generated after ":", in case it was shorter than max_tokens
-                    out_str = out_str.split(":")[-1]
+                        print("generated string: {}".format(out_str))
+                        print("targets: {}".format(targets))
 
-                    hits = None
-                    prop = None
-                    if (prompt == "cpl_any") or (prompt == "cnt"):
                         # check if any of the targets appear in the list
-                        hits = [t in out_str.split() for t in targets]
+                        output_list = [s.strip(".|,") for s in out_str.split()]
+                        hits = [t in output_list for t in targets]
                         prop = sum(hits)/len(hits)
-                    elif prompt == "cpl_first":
-                        # check if the first item appears anywhere in the output string
-                        hits = targets[0] in out_str.split()
 
-                    lst.append({
-                     "pref": prefix_key,
-                     "prompt": prompt,
-                     "len": len(targets),
-                     "string": out_str,
-                     "targets": targets,
-                     "n_correct": sum(hits),
-                     "p_correct": prop,
-                     "num_beams": num_beams,
-                     "top_p": top_p,
-                     "top_k": top_k}
-                    )
+                        lst.append({
+                         "pref": prefix_key,
+                         "prompt": prompt,
+                         "len": len(targets),
+                         "string": output_list,
+                         "targets": targets,
+                         "list": argins.input_list,
+                         "n_correct": sum(hits),
+                         "p_correct": prop,
+                         "do_sample": argins.do_sample,
+                         "num_beams": argins.num_beams,
+                         "top_p": argins.top_p,
+                         "top_k": argins.top_k,
+                         "temp": argins.temp}
+                        )
 
-                count += 1
+                    count += 1
 
 dfout = pd.DataFrame(columns=list(lst[0].keys()))
 out = dfout.append(lst, ignore_index=True)
 if text_generation:
-    outname = "./output/word_prediction_{}tok.txt".format(max_tokens)
+    outname = os.path.join("./output", argins.savename)
 else:
-    outname = "./output/word_prediction_1tok.txt"
+    outname = "./output/word_prediction_1tok.csv"
+
+#with open("outputs/word_prediction_{}tok_log.txt".format(argins.max_tokens), "w") as f:
+#    f.write("hello world!")
+#mlflow.log_artifacts(local_path="outputs/outputs/word_prediction_{}tok_log.txt")
+
 out.to_csv(path_or_buf=outname)
+
