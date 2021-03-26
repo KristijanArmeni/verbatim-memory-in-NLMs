424f0f1509774dcc58883e553e3c4b1a36c2d0a0
Kristijan Armeni <karmeni1@jh.edu>
2021-03-26 16:29:36 -0400

    added code to generate n-gram only string without the prompt and list repetition

--- data/stimuli2txt.py
+++ data/stimuli2txt.py
@@ -1,5 +1,3 @@
-
-
 from stimuli import prompts, prefixes
 import json
 import argparse
@@ -8,15 +6,23 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--json_filename", type=str)
-parser.add_argument("--scenario", choices=["sce1", "sce1rnd", "sce2", "rpt"], type=str)
-parser.add_argument("--condition", choices=["repeat", "permute", "control"], type=str)
+parser.add_argument("--scenario_key", choices=["sce1", "sce1rnd", "sce2", "rpt"], 
+                    default="sce1", type=str)
+parser.add_argument("--condition", choices=["repeat", "permute", "control"],
+                    default="repeat", type=str)
+parser.add_argument("--list_only", action="store_true")
 
 args = parser.parse_args()
 
 fname = args.json_filename
 
-outname = args.json_filename.replace(".json", "_{}_{}.txt".format(args.scenario, args.condition))
-markersoutname = args.json_filename.replace(".json", "_{}_{}_markers.txt".format(args.scenario, args.condition))
+if args.list_only: 
+    scenario = "nosce" 
+else: 
+    scenario = args.scenario_key
+
+outname = args.json_filename.replace(".json", "_{}_{}.txt".format(scenario, args.condition))
+markersoutname = args.json_filename.replace(".json", "_{}_{}_markers.txt".format(scenario, args.condition))
 
 trials = {
     "string": [],
@@ -33,17 +39,25 @@ if args.condition == "control":
 
     print("Creating reverse control condition...")
     print("Assuming input list can be evenly split into 3 lists each of len(list)==20!")
-    len3 = stim[0:20]
-    len5 = stim[20:40]
-    len10 = stim[40::]
+    
+    # test that we can divide by 3, 
+    # otherwise we sth has changed in the input lists
+    assert len(stim) % 3 == 0
+    
+    list_len = len(stim)//3
+    
+    len3 = stim[0:list_len]
+    len5 = stim[list_len:list_len*2]
+    len10 = stim[list_len*2::]
+    
     stim_reversed = [l for lst in (len3, len5, len10)
                        for l in reversed(lst)]
 
 input_lists = [", ".join(l) + "." for l in stim]
 
 # select dict with correct prompts and prefixes
-prompts = prompts[args.scenario]
-prefixes = prefixes[args.scenario]
+prompts = prompts[args.scenario_key]
+prefixes = prefixes[args.scenario_key]
 
 strings = []
 for prefix_key in prefixes.keys():
@@ -59,23 +73,31 @@ for prefix_key in prefixes.keys():
             else:
                 l2 = l
 
-            # tokenize each string separately, then make strings again
-            # and concatenate the strings together
+            # tokenize words and then join the list back to a string with spaces
+            # apparently neural-complexity-master/main.py is used with tokenized input.
+
             s1 = word_tokenize(prefixes[prefix_key])
             s2 = word_tokenize(l)
             s3 = word_tokenize(prompts[prompt])
             s4 = word_tokenize(l2)
 
+            # make exception for n-gram experiment
+            if args.list_only:
+                subparts = (s1, s2)  # use only word list, no prefixes ets
+            else:
+                subparts = (s1, s2, s3, s4)
+            
             # create coding for parts of trials
-            labels = [[i]*len(tokens) for i, tokens in enumerate((s1, s2, s3, s4))]
+            labels = [[i]*len(tokens) for i, tokens in enumerate(subparts)]
             labels_flattened = [item for sublist in labels for item in sublist]
-            # tokenize words and then join the list back to a string with spaces
-            # apparently neural-complexity-master/main.py is used with tokenized input.
-
-            string = " ".join(s1) + " " + \
-                     " ".join(s2) + " " + \
-                     " ".join(s3) + " " + \
-                     " ".join(s4)
+            
+            # construct the string
+            string = " ".join([" ".join(part) for part in subparts])
+            
+            #string = " ".join(s1) + " " + \
+            #         " ".join(s2) + " " + \
+            #         " ".join(s3) + " " + \
+            #         " ".join(s4)
 
             # quick check that all matches when spliting
             assert len(string.split()) == len(labels_flattened)
