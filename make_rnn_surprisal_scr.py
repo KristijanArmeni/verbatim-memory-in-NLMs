14c8d7a7d5310015fd26a71b600f36ed8cab757b
Kristijan Armeni <karmeni1@jh.edu>
2021-03-28 12:34:46 -0400

    update scripts that generate rnn .scr marcc scripts to loop over model instances as well

--- make_rnn_surprisal_scr.py
+++ make_rnn_surprisal_scr.py
@@ -1,62 +1,89 @@
 """
-script for construcing bash commands to be send as jubs to MARCC cluster
+script for construcing bash commands to be send as jobs to MARCC cluster
 """
+import os
 
-master_bash = open('run_rnn_surprisal_scripts.sh', 'w')
+# home directory on marcc compute cluster
+# this is git versioned repo:
+root_dir="~/code"
+scripts_dir = os.path.join(root_dir, "marcc_scripts")
 
-scenario = "sce1"
+master_bash = open(os.path.join(scripts_dir, 'run_rnn_surprisal_scripts.sh'), 'w')
 
-for scenario in ["sce1", "sce2", "sce1rnd"]:
-    for condition in ["repeat", "permute", "control"]:
-        for list_type in ["random", "categorized"]:
+# use hyphen to separate model id strings, this makes more sense in my naming
+# syntax, but replace with underscore for reading out the actual files
+model_ids = ["a-10", "b-11", "c-12", "d-13", "e-14"]
 
-            outname = "surprisal_rnn_{}_{}_{}.csv".format(scenario, condition, list_type)
+for model_id in model_ids:
+    for scenario in ["sce1", "sce2", "sce1rnd"]:
+        for condition in ["repeat", "permute", "control"]:
+            for list_type in ["random", "categorized"]:
+    
+                outname = "surprisal_rnn_{}_{}_{}_{}.csv".format(model_id, scenario, condition, list_type)
+                
+                # select a pretrained model from Van Schijndel et al (10.18653/v1/D19-1592)
+                # these hyperparameters made the most significant improvement in loss
+                # increasing further hasn't (see Table 1)
+                nhidden=2     # number of hidden layers
+                hiddendim=400 # dimensionality of hidden layer
+                textgb=40     # size of input data (M tokens)
+                                
+                model_file = "LSTM_{}_{}m_{}-d0.2.pt".format(hiddendim, 
+                                                             textgb, 
+                                                             model_id.replace("-", "_"))
+                model_path = os.path.join(root_dir, "rnn_models", model_file)
+                
+                # create absolute paths
+                python_script = os.path.join(root_dir, "neural-complexity-master", "main.py")
+                data_dir = os.path.join(root_dir, "data")
+                vocab_path = os.path.join(root_dir, "neural-complexity-master", "vocab.txt")
+                test_input_file = "{}_lists_{}_{}.txt".format(list_type, scenario, condition)
+                markers_fname = test_input_file.replace(".txt", "_markers.txt")
+                output_dir = os.path.join(root_dir, "output")
+    
+                # create command string
+                command = "python {} " \
+                          "--model_file {} " \
+                          "--vocab_file {} " \
+                          "--data_dir {} " \
+                          "--testfname {} " \
+                          "--csvfname {} " \
+                          "--markersfname {} " \
+                          "--output_dir {} " \
+                          "--lowercase --test --words" \
+                          .format(python_script,
+                                  model_path,
+                                  vocab_path,
+                                  data_dir,
+                                  test_input_file, 
+                                  outname, 
+                                  markers_fname,
+                                  output_dir)
+                
+                # construct script filename, open it and write commands
+                scr_filename = "script_rnn_{}_surprisal_{}_{}_{}".format(model_id, scenario, condition, list_type)
 
-            nhidden=2     # number of hidden layers
-            hiddendim=400 # dimensionality of hidden layer
-            textgb=40     # size of input data
-
-            model_file = "LSTM_{}_{}m_a_10-d0.2.pt".format(hiddendim, textgb)
-
-            # create filename with markers
-            test_input_file = "{}_lists_{}_{}.txt".format(list_type, scenario, condition)
-
-            markers_fname = test_input_file.replace(".txt", "_markers.txt")
-
-            # create command string
-            command = "python neural-complexity-master/main.py " \
-                      "--model_file ./rnn_models/{} " \
-                      "--vocab_file ./neural-complexity-master/vocab.txt " \
-                      "--data_dir ./data " \
-                      "--testfname {} " \
-                      "--csvfname {} " \
-                      "--markersfname {} " \
-                      "--output_dir ./output " \
-                      "--lowercase --test --words" \
-                      .format(model_file, test_input_file, outname, markers_fname)
-
-            scr_filename = "script_rnn_surprisal_{}_{}_{}".format(scenario, condition, list_type)
-            f = open(scr_filename + '.scr', 'w')
-
-            f.write("#!/bin/bash\n")
-            f.write("#SBATCH --job-name=" + scr_filename + "\n")
-            f.write("#SBATCH --time=48:0:0\n")
-            f.write("#SBATCH --partition=shared\n")
-            f.write("#SBATCH --nodes=1\n")
-            f.write("#SBATCH --ntasks-per-node=5\n")
-            f.write("#SBATCH --mail-type=end\n")
-            f.write("#SBATCH --mail-user=karmeni1@jhu.edu\n")
-            f.write("#SBATCH --output=" + scr_filename + ".log\n")
-            f.write("#SBATCH --error=" + scr_filename + ".err\n\n\n")
-
-            f.write("ml anaconda\n")
-
-            f.write("conda activate ~/code/conda_envs/lmpytorch1.3\n\n") # load environment with pytorch 1.3
-            f.write(command + "\n\n")  # write the python command to be executed
-            f.close()
-
-            print("Writing {}".format(scr_filename))
-            master_bash.write("sbatch " + scr_filename + ".scr\n")
+                f = open(os.path.join(scripts_dir, scr_filename) + '.scr', 'w')
+    
+                f.write("#!/bin/bash\n")
+                f.write("#SBATCH --job-name=" + scr_filename + "\n")
+                f.write("#SBATCH --time=48:0:0\n")
+                f.write("#SBATCH --partition=shared\n")
+                f.write("#SBATCH --nodes=1\n")
+                f.write("#SBATCH --ntasks-per-node=5\n")
+                f.write("#SBATCH --mail-type=end\n")
+                f.write("#SBATCH --mail-user=karmeni1@jhu.edu\n")
+                f.write("#SBATCH --output=" + scr_filename + ".log\n")
+                f.write("#SBATCH --error=" + scr_filename + ".err\n\n\n")
+    
+                f.write("ml anaconda\n")
+    
+                f.write("conda activate ~/code/conda_envs/lmpytorch1.3\n\n") # load environment with pytorch 1.3
+                f.write(command + "\n\n")  # write the python command to be executed
+                f.close()
+    
+                print("Writing {}".format(scr_filename))
+                master_bash.write("sbatch " + scr_filename + ".scr\n")
 
 master_bash.close()
 
