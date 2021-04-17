001eb5776c2272a6289cd76b8d04130478ae052e
KristijanArmeni <kristijan.armeni@gmail.com>
2021-04-17 12:10:32 -0400

    add homedir variable

--- data/make_stimuli.sh
+++ data/make_stimuli.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+homedir=$HOME"/code/lm-mem"
+
 for cond in "repeat" "control" "permute";
 do
     for sce in "sce1" "sce1rnd" "sce2" "sce3";
     do
         for l in "random" "categorized";
         do
-            python ./data/make_rnn_inputfiles.py --json_filename
-            "./data/"$l"_lists.json" --paradigm "with-context" --condition $cond --scenario_key $sce
+            python ./data/make_rnn_inputfiles.py --json_filename $homedir"/data/"$l"_lists.json" --paradigm "with-context" --condition $cond --scenario_key $sce
         done
     done
 done
