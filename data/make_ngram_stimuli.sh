4a67b9377147dcd0b52098e21fc0fa6883ffc845
KristijanArmeni <kristijan.armeni@gmail.com>
2021-05-04 08:21:34 -0400

    update to also generate categorized stimuli

--- data/make_ngram_stimuli.sh
+++ data/make_ngram_stimuli.sh
@@ -6,9 +6,9 @@ for cond in "repeat";
 do
     for sce in "sce1";
     do
-        for l in "ngram-random";
+        for l in "ngram-random" "ngram-categorized";
         do
-            python $homedir"/data/make_rnn_inputfiles.py" --json_filename $homedir"/data/"$l"_lists.json" --paradigm "repeated-ngrams" --condition $cond --scenario_key $sce
+            python $homedir"/data/make_rnn_inputfiles.py" --json_filename $homedir"/data/"$l".json" --paradigm "repeated-ngrams" --condition $cond --scenario_key $sce
         done
     done
 done
