3a3e41aa774c4307a1b2afea0609ca7a8691b485
KristijanArmeni <kristijan.armeni@gmail.com>
2021-06-14 11:53:57 -0400

    update paths

--- dataset_script.sh
+++ dataset_script.sh
@@ -12,11 +12,12 @@ ml anaconda
 conda activate ~/code/conda_envs/core_env
 
 sourcedir=$HOME/code/lm-mem
+datadir=$HOME/work/karmeni1/wikitext-103
 
-python $sourcedir/dataset.py --train_tokens wiki.train.tokens_40m \
-                             --valid_tokens wiki.valid.tokens \
-                             --test_tokens wiki.test.tokens \
-                             --train_tokenizer True \
-                             --tokenizer_train_tokens wiki.train.tokens \
+python $sourcedir/dataset.py --train_tokens $datadir/wiki.train.tokens_40m \
+                             --valid_tokens $datadir/wiki.valid.tokens \
+                             --test_tokens $datadir/wiki.test.tokens \
+                             --train_tokenizer \
+                             --tokenizer_train_tokens $datadir/wiki.train.tokens \
                              --tokenizer_savedir $HOME/work/karmeni1/lm-mem/gpt2_wikitext103 \
-                             --saveidr $HOME/work/karmeni1/wikitext-103
+                             --saveidr $datadir
