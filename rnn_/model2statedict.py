5792fb9e149e321e91f6785b393256a00b1fb4f0
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2021-11-06 00:27:27 +0100

    modernized model2statedict

--- rnn_/model2statedict.py
+++ rnn_/model2statedict.py
@@ -2,27 +2,43 @@
 """
 Created on Fri Aug  6 17:11:45 2021
 
-@author: karmeni1
+@authors: karmeni1, Gabriel Kressin
 """
 
 import os
-import model
 import torch
+import argparse
 
-source_dir = "C:\\Users\\karmeni1\\project\\lm-mem\\data\\checkpoints\\lstm"
-checkpoints = ['LSTM_800_80m_a_15-d0.2.pt']
 
-# do the work
-
-for ckpname in checkpoints:
-
-    in_fullfile = os.path.join(source_dir, ckpname)
-    model_object = torch.load(in_fullfile, map_location="cpu")
+def convert_model(model_file):
+    model_object = torch.load(model_file, map_location="cpu")
 
     # check if filename is provided, else append weights to input file name for saving
-    output_fname = ckpname.replace(".pt", "_statedict.pt")
+    output_fname = model_file.replace(".pt", "_statedict.pt")
 
     # save the state dict only
-    out_fullfile = os.path.join(source_dir, output_fname)
-    print("Saving {}".format(out_fullfile))
-    torch.save(model_object.state_dict(), out_fullfile)
+    print("Saving {}".format(output_fname))
+    torch.save(model_object.state_dict(), output_fname)
+
+
+def convert_models(model):
+    """Iterates (if possible) through dir and converts all models to statedicts"""
+    if os.path.isdir(model):
+        for model_file in os.listdir(model):
+            if "state_dict" not in model_file:
+                convert_model(os.path.join(model, model_file))
+    else:
+        convert_model(model)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="converts pytorch model files into their state_dict")
+    parser.add_argument("model", type=str,
+        help="path/to/models containing multiple lstm.pt files or single path/to/model.pt")
+
+    args = parser.parse_args()
+
+    convert_models(
+        model=args.model
+    )
