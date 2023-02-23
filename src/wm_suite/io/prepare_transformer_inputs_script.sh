c79cf14892e9c49dee486acfdf6910919c39cad1
KristijanArmeni <kristijan.armeni@gmail.com>
2023-02-23 10:15:32 -0500

    - just change some param values, this script should prob be moved elswhere

--- src/wm_suite/io/prepare_transformer_inputs_script.sh
+++ src/wm_suite/io/prepare_transformer_inputs_script.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-scenario="sce1"
-model_id="trf-wt103"
+scenario="sce5"
+model_id="gpt2"
 
 # bert stimuli
-for list_type in "categorized" "random";
+for list_type in "random";
 do
 
-    for condition in "repeat" "permute" "control";
+    for condition in "repeat";
     do
 
-        for list_len in "n3" "n5" "n7" "n10";
+        for list_len in "n3";
         do
 
-            for prompt_key in "1" "2" "3" "4" "5";
+            for prompt_key in "1";
             do
 
                 output_name=$model_id"_"$condition"_"$scenario"_"$prompt_key"_"$list_len"_"$list_type
@@ -22,10 +22,10 @@ do
                                                     --condition $condition \
                                                     --prompt_key $prompt_key \
                                                     --list_len $list_len \
-                                                    --path_to_tokenizer "/home/ka2773/project/lm-mem/data/wikitext-103_tokenizer" \
+                                                    --path_to_tokenizer "gpt2" \
                                                     --device "cuda" \
                                                     --input_filename $list_type"_lists.json" \
-                                                    --output_dir "./transformer_input_files/transformer_wt103" \
+                                                    --output_dir "/home/ka2773/project/lm-mem/src/data/transformer_input_files/gpt2" \
                                                     --output_filename $output_name
 
             done
