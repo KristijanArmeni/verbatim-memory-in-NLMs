c7f4dbb921baa153840fc17964e0d4539fec3085
KristijanArmeni <kristijan.armeni@gmail.com>
2023-04-14 11:04:30 -0400

    - update imports and move argument parser within main

--- src/wm_suite/viz/make_figures.py
+++ src/wm_suite/viz/make_figures.py
@@ -1,52 +1,62 @@
 
 import os
 import logging
-import timecourse_figure
-import list_length_figure
-import context_length_figure
-import context_structure_figure
-import layer_figure
-import vignette_control_figure
+import argparse
+from wm_suite.viz import timecourse_figure
+from wm_suite.viz import list_length_figure
+from wm_suite.viz import context_length_figure
+from wm_suite.viz import context_structure_figure
+from wm_suite.viz import layer_figure
+from wm_suite.viz import vignette_control_figure
 import logging
-from func import set_manuscript_style
+from wm_suite.viz.func import set_manuscript_style
 
 
-def main(model, figpath):
+def main(input_args=None):
 
-    set_manuscript_style()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, choices=["gpt2", "awd_lstm", "w-12v2"])
+    parser.add_argument("--savedir", type=str)
+
+    if input_args is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(input_args)
+
+    #set_manuscript_style()
 
     ##### ===== TIME COURSE PLOT ===== #####
-    logging.info(f"Making timecourse figure for model {model}")
-    timecourse_figure.main(["--model", model, "--savedir", figpath])
+    logging.info(f"Making timecourse figure for args.model {args.model}")
+    timecourse_figure.main(["--model", args.model, "--savedir", args.savedir])
 
 
     ##### ===== LIST LENGTH PLOT ===== #####
-    logging.info(f"Making list length figure for model {model}")
-    list_length_figure.main(["--model", model, "--savedir", figpath])
+    logging.info(f"Making list length figure for model {args.model}")
+    list_length_figure.main(["--model", args.model, "--savedir", args.savedir])
 
 
     ##### ===== CONTEXT LENGTH PLOT ===== #####
-    logging.info(f"Making context length figure for model {model}")
-    context_length_figure.main(["--model", model, "--savedir", figpath])
+    logging.info(f"Making context length figure for model {args.model}")
+    context_length_figure.main(["--model", args.model, "--savedir", args.savedir])
 
 
     ##### ===== CONTEXT STRUCTURE PLOT ===== #####
-    logging.info(f"Making context structure figure for model {model}")
-    context_structure_figure.main(["--model", model, "--savedir", figpath])
+    logging.info(f"Making context structure figure for model {args.model}")
+    context_structure_figure.main(["--model", args.model, "--savedir", args.savedir])
 
 
     ##### ===== LAYER PLOT ===== #####
     for model_id in ["w-01v2", "w-03v2", "w-06v2", "w-12v2"]:
 
         logging.info(f"Making layer figure for model {model_id}")
-        layer_figure.main(["--model_id", model_id, "--savedir", figpath])
+        layer_figure.main(["--model_id", model_id, "--savedir", args.savedir])
 
 
     ##### ===== CONTROL VIGNETTES ===== #####
     for exp in ["punctuation", "name", "shuffled-preface", "shuffled-prompt"]:
 
-        logging.info(f"Making control figure ({exp}) for model {model}")
-        vignette_control_figure.main(["--model", f"{model}", "--experiment", exp, "--savedir", figpath])
+        logging.info(f"Making control figure ({exp}) for model {args.model}")
+        vignette_control_figure.main(["--model", f"{args.model}", "--experiment", exp, "--savedir", args.savedir])
 
     logging.info("\n##### ===== DONE ===== #####")
 
@@ -55,14 +65,6 @@ def main(model, figpath):
 
 if __name__ == "__main__":
 
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, choices=["gpt2", "awd_lstm", "w-12v2"])
-    parser.add_argument("--savedir", type=str)
-
-    args = parser.parse_args()
-
-    main(args.model, args.savedir)
+    main()
 
 
