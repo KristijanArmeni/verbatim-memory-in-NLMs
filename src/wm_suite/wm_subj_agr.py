- fix to read in correct input files - code to get baseline accuracy - attention masking and input masking - log for output csv - bunch of logging

src/wm_suite/wm_subj_agr.py
@@ -5,22 +5,26 @@ from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 from typing import Dict, Tuple
 import torch
 import logging
+from typing import List, Tuple, Dict
 from tqdm import tqdm
 from itertools import product
+from wm_suite.wm_ablation import ablate_attn_module
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 LINZEN2016 = "/scratch/ka2773/project/lm-mem/sv_agr/linzen2016/linzen2016_english.json"
-LAKRETZ2021_SHORT = "/scratch/ka2773/project/lm-mem/sv_agr/lakretz2021/short_nested_inner_english.json"
-LAKRETZ2021_LONG = "/scratch/ka2773/project/lm-mem/sv_agr/lakretz2021/long_nested_inner_english.json"
+LAKRETZ2021_SHORT = "/scratch/ka2773/project/lm-mem/sv_agr/lakretz2021/short_nested_outer_english.json"
+LAKRETZ2021_LONG = "/scratch/ka2773/project/lm-mem/sv_agr/lakretz2021/long_nested_outer_english.json"
 
+# define dicts that hold input arguments to create ablated models
+all_heads = list(range(12))
+single_layers = [[i] for i in list(range(12))]
+layers_dict = {"".join([str(e) for e in a]): a for a in single_layers + [[0,1], [2, 3], [0, 1, 2, 3], [8, 9], [10, 11], [8, 9, 10, 11]]}
 
-WIEGHTS_DIR = "/scratch/ka2773/project/lm-mem/output/ablation/zero_qk"
-ABLATION_KEYS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "01", "23", "0123", "89", "1011", "891011", "all"]
-
-MODEL_WEIGTHS = {f"ablate-{infix}": os.path.join(WIEGHTS_DIR, f"gpt2_ablate-{infix}-all.pt") for infix in ABLATION_KEYS}
-MODEL_WEIGTHS["unablated"] = "gpt2"
-MODEL_WEIGTHS["rand-init"] = "rand-init"
+ablation_dict = {f"ablate-{key}": {"layers": layers_dict[key], "heads": all_heads} for key in layers_dict}
+ablation_dict["ablate-all"] = {'layers': list(range(12)), "heads": all_heads}
+ablation_dict["rand-init"] = "rand-init"
+ablation_dict["unablated"] = "gpt2"
 
 
 def read_json_data(path: str) -> Dict:  
@@ -61,7 +65,7 @@ def get_agreement_score(logits: torch.Tensor, targets: Tuple, tokenizer) -> int:
 
     # make sure we add space as a word marker to the string itself
     # otherwise, the BPE tokenizer assumes the string is attached to another strin and BPE splits it
-    corr_id = tokenizer.encode(" " + tar[0])
+    corr_id = tokenizer.encode(" " + tar[0])    # the first item in the <tar> list is always the correct one
     incorr_id = tokenizer.encode(" " + tar[1])
 
     # if there's more than to IDs, it means the verb was BPE-split, skip it
@@ -71,29 +75,47 @@ def get_agreement_score(logits: torch.Tensor, targets: Tuple, tokenizer) -> int:
         out = {}
 
     else:
-        strings = tuple(tokenizer.decode(id) for id in [corr_id, incorr_id])
 
+        # observed probabilities
         prob_correct = probs[corr_id].item()
         prob_incorrect = probs[incorr_id].item()
-        
+
+        agreement_correct = int(prob_correct > prob_incorrect)
+
         #print(strings)
         #print((prob_correct, prob_incorrect))
+        strings = tuple(tokenizer.decode(id) for id in [corr_id, incorr_id])
         out = {key: v for key, v in zip(strings, (prob_correct, prob_incorrect))}
-        out['res'] = int(prob_correct > prob_incorrect)
+
+        out['res'] = agreement_correct
         out['max'] = torch.max(probs).cpu().item()
         out['corr_percentile'] = (torch.sum(probs > prob_correct).cpu().item())/len(probs)
 
     return out
 
 
-def collect_agreement_scores(model, inputs, targets, tokenizer):
+def collect_agreement_scores(model, inputs, targets, attention_mask_positions, tokenizer):
+    """
+    Parameters:
+    ----------
+    model : transformers.PreTrainedTransformer
+    inputs : torch.tensor, shape = (n_samples, sequence_len)
+    """
 
     scores = []
+    
+    # set attention masking if requested
+    attention_mask = None
+    if attention_mask_positions is not None:
 
+        logging.info(f"Attention masking position {attention_mask_positions} in sequences")
+        
+        attention_mask = torch.ones(size=(1, len(inputs[0])), device=model.device)
+        attention_mask[0, torch.tensor(attention_mask_positions)] = 0
+        
     for inp, tgt in tqdm(zip(inputs, targets), total=len(inputs)):
 
-        model.eval()
-        o = model(input_ids=inp)
+        o = model(input_ids=inp, attention_mask=attention_mask)
 
         s = get_agreement_score(o.logits[-1, :], targets=tgt, tokenizer=tokenizer)
 
@@ -102,15 +124,36 @@ def collect_agreement_scores(model, inputs, targets, tokenizer):
     return scores
 
 
-def get_accuracy(scores):
+def get_accuracy(scores: Dict) -> float:
 
-    res = [e['res'] for e in scores if bool(e)]
+    res = [e['res'] for e in scores if bool(e)]  # if <e> is empty, it means the targets were BPE split, we skip these cases
     return round(sum(res)/len(res), 4)
 
 
-def run_experiment(dataset_path, model, tokenizer, comment, experiment_id):
+def inputs2tensor(inputs, tokenizer):
+    return torch.LongTensor([tokenizer.encode(x) for x in inputs])
+
+
+def mask_inputs(inputs: torch.Tensor, masked_positions: List, masked_value: str):
+    
+    logging.info(f"Masking input as positions {masked_positions} with {masked_value}")
+    out = []
+    for i, inp in enumerate(inputs):
+        tokens = inp.split(" ")
+        for pos in masked_positions:
+            tokens[pos] = masked_value
+
+        out.append(" ".join(tokens))
+
+    return out
+
+
+def run_experiment(dataset_path, model, tokenizer, cfg, comment, experiment_id):
+
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model.eval()
 
     examples = read_json_data(dataset_path)
 
@@ -119,20 +162,50 @@ def run_experiment(dataset_path, model, tokenizer, comment, experiment_id):
     else:
         inputs, targets = get_inputs_and_targets(examples_list=examples, comment=None)
 
-    def inputs2tensor(inputs):
-        return [torch.LongTensor(tokenizer.encode(x)).to(device) for x in inputs]
-
+    logging.info("Collecting agreement scores...")
+    logging.info(f"Example input: {inputs[5]}")
     scores = collect_agreement_scores(model.to(device), 
-                                      inputs=inputs2tensor(inputs), 
+                                      inputs=inputs2tensor(inputs, tokenizer).to(device), 
                                       targets=targets,
+                                      attention_mask_positions=None,
                                       tokenizer=tokenizer)
+
+
+    logging.info("Collecting baseline agreement scores")
+        
+    mask_positions = [1, 4]  # masking out the first and the second noun caryying grammatical number information
+    if comment in ["singular_singular_singular", "plural_plural_plural", "singular_singular_plural", "plural_plural_singular"]:
+        mask_positions = [1, 4, 7]
+
+
+    if cfg['input_masking']:
+
+        inputs = mask_inputs(inputs=inputs, 
+                             masked_positions=mask_positions,
+                             masked_value=tokenizer.unk_token)
+        logging.info(f"Using masked inputs for baseline")
+        logging.info(f"Example baseline input: {inputs[5]}")
+
+
+    baseline_scores = collect_agreement_scores(model.to(device), 
+                                               inputs=inputs2tensor(inputs, tokenizer).to(device), 
+                                               targets=targets,
+                                               attention_mask_positions=mask_positions,
+                                               tokenizer=tokenizer)
 
     output = {}
     output["scores"] = scores
     output["accuracy"] = get_accuracy(scores)
+    output["accuracy_baseline"] = get_accuracy(baseline_scores)
+    output["accuracy_adj"] = get_accuracy(scores) - get_accuracy(baseline_scores)
     output["dataset"] = dataset_path
     output["type"] = comment
     output['id'] = experiment_id
+    output["cfg"] = cfg
+
+    logging.info("\nRESULTS")
+    logging.info(f"\n{comment}:\naccuracy {output['accuracy']} | baseline {output['accuracy_baseline']} | adjusted {output['accuracy_adj']}\n")
+    
 
     return output
 
@@ -153,7 +226,7 @@ def main(input_args=None):
     torch.manual_seed(54321)
 
     dataset_dict = {
-        "linzen2016": {'path': LINZEN2016, 'comment': None}, 
+        #"linzen2016": {'path': LINZEN2016, 'comment': None}, 
         "lakretz2021_S_A": {'path': LAKRETZ2021_SHORT, 'comment': "singular_singular"},
         "lakretz2021_S_B": {'path': LAKRETZ2021_SHORT, 'comment': "singular_plural"},
         "lakretz2021_S_C": {'path': LAKRETZ2021_SHORT, 'comment': "plural_singular"},
@@ -164,24 +237,53 @@ def main(input_args=None):
         "lakretz2021_L_D": {'path': LAKRETZ2021_LONG, 'comment': "plural_plural_singular"},
     }
 
-    datasets = list(dataset_dict.keys())
-    checkpoints = list(MODEL_WEIGTHS.keys())
+    long_deps = ["singular_singular_singular", "singular_singular_plural", "plural_plural_plural", "plural_plural_singular"]
+    short_labels = {
+        "singular_singular": "SS",
+        "singular_plural": "SP",
+        "plural_singular": "PS",
+        "plural_plural": "PP",
+        "singular_singular_singular": "SSS",
+        "singular_singular_plural": "SSP",
+        "plural_plural_plural": "PPP",
+        "plural_plural_singular": "PPS",
+    }
 
-    combinations = product(datasets, checkpoints)
+    datasets = list(dataset_dict.keys())
+    model_names = list(ablation_dict.keys())
+
+    combinations = list(product(datasets, model_names))
+
+    # this log will go into a dataframe for compact storing of main results
+    log = {
+        "deplen": [],     # long vs. short
+        "agr": [],        # agreement condition
+        "model": [],      # unablated etc.
+        "acc": [],        # raw accuracy
+        "acc0_A": [],     # baseline accuracy (attention masking only)
+        "acc0_B": [],     # baseline accuracy (attention and input masking)
+    }
 
     # run every model on both datasets
-    for data_checkpoint_tuple in combinations:
+    for data_model_name_tuple in combinations:
 
-        dataset_key, checkpoint = data_checkpoint_tuple
+        dataset_key, model_name = data_model_name_tuple
 
-        if checkpoint == "unablated":
+        ds_path = dataset_dict[dataset_key]['path']
+        comment = dataset_dict[dataset_key]['comment']
+
+        log['model'].append(model_name)
+        log['agr'].append(short_labels[comment])
+        log['deplen'].append("long" if comment in long_deps else "short")
+
+        if model_name == "unablated":
 
             logging.info(f"Loading pretrained gpt2...")
             
             model = GPT2LMHeadModel.from_pretrained("gpt2")
             tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 
-        elif checkpoint == "rand-init":
+        elif model_name == "rand-init":
 
             logging.info(f"Loading randomly initialized gpt2...")
 
@@ -191,29 +293,68 @@ def main(input_args=None):
             tokenizer =  GPT2TokenizerFast.from_pretrained("gpt2")
 
         else:
-            
-            logging.info(f"Loading {MODEL_WEIGTHS[checkpoint]}...")
-            config = GPT2LMHeadModel.from_pretrained("gpt2").config
-            model = GPT2LMHeadModel(config)
-            model.load_state_dict(torch.load(MODEL_WEIGTHS[checkpoint]))
+
+            # ablate the model
+            model = GPT2LMHeadModel.from_pretrained("gpt2")
+            model = ablate_attn_module(model, 
+                                       layers=ablation_dict[model_name]['layers'],
+                                       heads=ablation_dict[model_name]['heads'],
+                                       ablation_type="zero")
 
             tokenizer =  GPT2TokenizerFast.from_pretrained("gpt2")
         
+
         # ===== RUN EXPERIMENT ===== #
-        logging.info(f"Running experiment for: {dataset_key} | combination: {dataset_dict[dataset_key]['comment']}")
-        outputs = run_experiment(dataset_path=dataset_dict[dataset_key]['path'],
+        logging.info("\n==== STARTING EXPERIMENTS =====\n")
+
+        logging.info(f"Running experiment for:\n" +
+                     f"dataset: {dataset_key}\n" + 
+                     f"combination: {comment}\n" +
+                     f"model: {model_name}")
+
+        outputs1 = run_experiment(dataset_path=ds_path,
                                  model=model,
                                  tokenizer=tokenizer,
-                                 comment=dataset_dict[dataset_key]['comment'],
-                                 experiment_id=f"{checkpoint}_{dataset_key}"
+                                 cfg={"input_masking": False},
+                                 comment=comment,
+                                 experiment_id=f"{model_name}_{dataset_key}"
                                 )
 
+        log['acc'].append(outputs1['accuracy'])
+        log['acc0_A'].append(outputs1['accuracy_baseline'])
+   
         # save outputs
-        savename = os.path.join(args.savedir, f"sva_{checkpoint}_{dataset_key}.json")
-        logging.info(f"Saving {savename}")
-        with open(savename, 'w') as fh:
-            json.dump(outputs, fh, indent=4)
+        savename = os.path.join(args.savedir, f"sva_{model_name}_{dataset_key}.json")
+        logging.info(f"Saving {savename}\n")
+        #with open(savename, 'w') as fh:
+        #    json.dump(outputs1, fh, indent=4)
 
+        logging.info(f"Running input masking experiment for: {dataset_key} | combination: {dataset_dict[dataset_key]['comment']}")
+        outputs2 = run_experiment(dataset_path=ds_path,
+                                 model=model,
+                                 tokenizer=tokenizer,
+                                 cfg={"input_masking": True},
+                                 comment=comment,
+                                 experiment_id=f"{model_name}_{dataset_key}"
+                                )
+
+        log['acc0_B'].append(outputs2['accuracy_baseline'])
+
+        # save outputs
+        savename = os.path.join(args.savedir, f"sva_{model_name}_{dataset_key}_input-masking.json")
+        logging.info(f"Saving {savename}\n")
+        #with open(savename, 'w') as fh:
+        #    json.dump(outputs2, fh, indent=4)
+
+        logging.info("\n===== DONE =====\n")
+
+    df = pd.DataFrame(log)
+
+    savename = os.path.join(args.savedir, f"ablation_sva.csv")
+    logging.info(f"Saving {savename}\n")
+    df.to_csv(savename, sep="\t")
+
+    logging.info("===== END OF RUN =====\n")
 
 if __name__ == "__main__":
 