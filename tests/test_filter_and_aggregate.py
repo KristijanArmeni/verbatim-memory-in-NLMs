79840bacd9d253bd5c324b0cdf654e0f8a3ae1a7
KristijanArmeni <kristijan.armeni@gmail.com>
2023-06-08 16:12:38 -0400

    - wrap into a function and adjust imports

--- tests/test_filter_and_aggregate.py
+++ tests/test_filter_and_aggregate.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 import numpy as np
-from src.src.wm_suite.viz.func import filter_and_aggregate
+from src.wm_suite.viz.func import filter_and_aggregate
 
 
 def generate_sequence(tokens, val1, val2):
@@ -13,64 +13,67 @@ def generate_sequence(tokens, val1, val2):
     
     return tokens, values1
 
-t1, v1 = generate_sequence(["door", "roof", "candle"], 8., 4.)
-t2, v2 = generate_sequence(["car", "shop", "window"], 8., 8.)
-t3, v3 = generate_sequence(["house", "spoon", "north"], 8., 0.)
 
-stimid = np.repeat([1, 2, 3], len(t1))
-marker = np.tile([0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3], 3)
-marker_pos_rel = np.tile([-3, -2, -1, np.nan, 0, 1, 2, np.nan, -4, -3, -2, -1, 0, 1, 2, np.nan], 3)
+def test_filter_and_aggregate():
 
-rec = {"token": t1 + t2 + t3, "surp": v1 + v2 + v3, "stimid": stimid, "marker": marker, "marker_pos_rel": marker_pos_rel}
+    t1, v1 = generate_sequence(["door", "roof", "candle"], 8., 4.)
+    t2, v2 = generate_sequence(["car", "shop", "window"], 8., 8.)
+    t3, v3 = generate_sequence(["house", "spoon", "north"], 8., 0.)
 
-df = pd.DataFrame(rec)
+    stimid = np.repeat([1, 2, 3], len(t1))
+    marker = np.tile([0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3], 3)
+    marker_pos_rel = np.tile([-3, -2, -1, np.nan, 0, 1, 2, np.nan, -4, -3, -2, -1, 0, 1, 2, np.nan], 3)
 
-# add other expected columns in df
+    rec = {"token": t1 + t2 + t3, "surp": v1 + v2 + v3, "stimid": stimid, "marker": marker, "marker_pos_rel": marker_pos_rel}
 
-df['model'] = "gpt2"
-df['second_list'] = "repeat"
-df['list'] = "random"
-df['prompt_len'] = 8
-df["list_len"] = 3
-df['context'] = "intact"
-df['model_id'] = "test"
+    df = pd.DataFrame(rec)
 
-# grad the first time step (technically, mean computation is applied, but no effective here)
-variables = [{"list_len": [3]},
-             {"prompt_len": [8]},
-             {"context": ["intact"]},
-             {"marker_pos_rel": [0, 1, 2]}]
+    # add other expected columns in df
 
-# run the tested function
-df2, dagg_ = filter_and_aggregate(df, 
-                                model="gpt2", 
-                                model_id=df.model_id.unique().item(), 
-                                groups=variables, 
-                                aggregating_metric="mean")
+    df['model'] = "gpt2"
+    df['second_list'] = "repeat"
+    df['list'] = "random"
+    df['prompt_len'] = 8
+    df["list_len"] = 3
+    df['context'] = "intact"
+    df['model_id'] = "test"
 
-repeat_surp = df2.x_perc.to_numpy()
+    # grad the first time step (technically, mean computation is applied, but no effective here)
+    variables = [{"list_len": [3]},
+                {"prompt_len": [8]},
+                {"context": ["intact"]},
+                {"marker_pos_rel": [0, 1, 2]}]
 
-# check that repeat surprisals are as expected
-assert repeat_surp[0] == 50
-assert repeat_surp[1] == 100
-assert repeat_surp[2] == 0
+    # run the tested function
+    df2, dagg_ = filter_and_aggregate(df, 
+                                    model="gpt2", 
+                                    model_id=df.model_id.unique().item(), 
+                                    groups=variables, 
+                                    aggregating_metric="mean")
 
-# grad the first time step (technically, mean computation is applied, but no effective here)
-variables = [{"list_len": [3]},
-             {"prompt_len": [8]},
-             {"context": ["intact"]},
-             {"marker_pos_rel": [0]}]
+    repeat_surp = df2.x_perc.to_numpy()
 
-# run the tested function
-df2, dagg_ = filter_and_aggregate(df, 
-                                model="gpt2", 
-                                model_id=df.model_id.unique().item(), 
-                                groups=variables, 
-                                aggregating_metric="mean")
+    # check that repeat surprisals are as expected
+    assert repeat_surp[0] == 50
+    assert repeat_surp[1] == 100
+    assert repeat_surp[2] == 0
 
-repeat_surp = df2.x_perc.to_numpy()
+    # grad the first time step (technically, mean computation is applied, but no effective here)
+    variables = [{"list_len": [3]},
+                {"prompt_len": [8]},
+                {"context": ["intact"]},
+                {"marker_pos_rel": [0]}]
 
-# check that repeat surprisals are as expected
-assert repeat_surp[0] == 50
-assert repeat_surp[1] == 100
-assert repeat_surp[2] == 0
+    # run the tested function
+    df2, dagg_ = filter_and_aggregate(df, 
+                                    model="gpt2", 
+                                    model_id=df.model_id.unique().item(), 
+                                    groups=variables, 
+                                    aggregating_metric="mean")
+
+    repeat_surp = df2.x_perc.to_numpy()
+
+    # check that repeat surprisals are as expected
+    assert repeat_surp[0] == 50
+    assert repeat_surp[1] == 100
+    assert repeat_surp[2] == 0
