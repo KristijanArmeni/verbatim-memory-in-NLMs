6395f609bea7bb44ab883851c72a5ee2bd48f7f0
KristijanArmeni <kristijan.armeni@gmail.com>
2023-06-08 10:27:47 -0400

    - esthetic updates - removed patches in image - reset ticks on line plot - changed eos token symbol

--- src/wm_suite/viz/ablation/attn_weights_example.py
+++ src/wm_suite/viz/ablation/attn_weights_example.py
@@ -35,7 +35,7 @@ def make_example_plot(ax: np.ndarray, x: np.ndarray, d: Dict, query_id: int, lay
 
     i = sequence
     l = layer
-    im = ax[1].imshow(x[i, 0:query_id, :, l].T, aspect='auto', cmap="Greys")
+    im = ax[1].imshow(x[i, 0:query_id, :, l].T, aspect='auto', vmin=0, vmax=1, cmap="Greys")
 
     labelfs = 16  # fontsize of axis and tick labels
 
@@ -46,10 +46,14 @@ def make_example_plot(ax: np.ndarray, x: np.ndarray, d: Dict, query_id: int, lay
     # line plot
     m = np.mean(x[i, 0:query_id, :, l], axis=1)
 
-    ax[0].plot(np.arange(query_id), m, 'o--', markersize=7, mec='white')
+    ax[0].plot(np.arange(query_id), m, 'o--', markersize=8, mec='white')
+    
+    ax[0].set_yticks([0, 0.1, 0.2, 0.3])
+    ax[0].set_yticklabels([0, 0.1, 0.2, 0.3])
+    ax[0].tick_params(labelsize=labelfs-1)
 
     ax[0].set_ylabel('Avg. attention\nweight', fontsize=labelfs)
-    ax[0].tick_params(labelsize=labelfs)
+
     ax[0].grid(visible=True, linewidth=0.5)
     
     # despine axis 1
@@ -57,7 +61,8 @@ def make_example_plot(ax: np.ndarray, x: np.ndarray, d: Dict, query_id: int, lay
     ax[0].spines['right'].set_visible(False)
     
     ylabels = [s.strip("Ġ") for s in d['tokens'][i][0:query_id]]
-    ax[1].set_xticklabels(labels=ylabels, ha='right', rotation=55, fontsize=labelfs)
+    ylabels = [s.replace("<|endoftext|>", "<eos>") for s in ylabels]
+    ax[1].set_xticklabels(labels=ylabels, ha='center', rotation=90, fontsize=labelfs)
     ax[1].set_yticks(np.arange(0, 12, 2))
     ax[1].set_yticklabels(np.arange(1, 12, 2), fontsize=labelfs)
 
@@ -66,14 +71,14 @@ def make_example_plot(ax: np.ndarray, x: np.ndarray, d: Dict, query_id: int, lay
     cue1 = patches.Rectangle((12.5, -0.5), 1, 12, linewidth=2, ec='tab:red', facecolor='none', zorder=2)
     early_window_rect = patches.Rectangle((13.5, -0.5), 5, 12, linewidth=2, edgecolor='tab:blue', facecolor='none', zorder=2)
     preceding_window_rect = patches.Rectangle((query_id-4.5, -0.5), 3, 12, linewidth=2, edgecolor='tab:orange', facecolor='none', zorder=1)
-    for patch in (cue1, rect, early_window_rect):
-        ax[1].add_patch(patch)
+    #for patch in (cue1, rect, early_window_rect):
+    #    ax[1].add_patch(patch)
     
-    patch_height = 0.29
-    cue2 = patches.Rectangle((query_id-1.5, 0), 1, patch_height, linewidth=2, edgecolor='tab:red', facecolor='tab:red', alpha=0.2, zorder=2)
-    cue1 = patches.Rectangle((12.5, 0), 1, patch_height, linewidth=2, ec='tab:red', facecolor='tab:red', alpha=0.2, zorder=2)
-    early_window_rect = patches.Rectangle((13.5, 0), 5, patch_height, linewidth=2, edgecolor='tab:blue', facecolor='tab:blue', alpha=0.2, zorder=2)
-    preceding_window_rect = patches.Rectangle((query_id-4.5, 0), 3, patch_height, linewidth=2, edgecolor='tab:orange', facecolor='tab:orange', alpha=0.2, zorder=2)
+    patch_height = 0.3
+    cue2 = patches.Rectangle((query_id-1.5, 0), 1, patch_height, linewidth=2, facecolor='tab:red', alpha=0.2, zorder=2)
+    cue1 = patches.Rectangle((12.5, 0), 1, patch_height, linewidth=2, facecolor='tab:red', alpha=0.2, zorder=2)
+    early_window_rect = patches.Rectangle((13.5, 0), 5, patch_height, linewidth=2, facecolor='tab:blue', alpha=0.2, zorder=2)
+    preceding_window_rect = patches.Rectangle((query_id-4.5, 0), 3, patch_height, linewidth=2, facecolor='tab:orange', alpha=0.2, zorder=2)
     for patch in (cue1, early_window_rect, cue2):
         ax[0].add_patch(patch)
 
@@ -84,17 +89,76 @@ def make_example_plot(ax: np.ndarray, x: np.ndarray, d: Dict, query_id: int, lay
     ax[0].annotate("", xy=(16, 0.33), xytext=(query_id-1.5, 0.33), 
                   arrowprops={"arrowstyle": "->", 'connectionstyle': "arc3,rad=0.1"})
 
-    ax[0].text(x=22, y=0.33, s="attention towards tokens in repeated sequences", fontsize=14)
+    ax[0].text(x=22, y=0.33, s="Attention towards tokens in repeated sequences", fontsize=14)
 
-    cax = ax[1].inset_axes([1.04, 0.15, 0.01, 0.7])
+    cax = ax[1].inset_axes([1.02, 0, 0.01, 1])
     cbar = plt.colorbar(im, ax=ax[1], cax=cax, shrink=0.5, anchor=(0.5, 1))
 
     cbar.ax.get_yaxis().labelpad = 10
-    cbar.ax.set_ylabel("Attention\nweight", rotation=90, fontsize=labelfs)
+    cbar.ax.set_ylabel("Attention weight", rotation=90, fontsize=labelfs)
     cbar.ax.tick_params(labelsize=labelfs)
 
 
     return ax
+
+
+def replace_nouns(x, replacements):
+   d = {"patience": replacements[0], "notion": replacements[1], "movie": replacements[2]}
+   return [d[e] if e in d.keys() else e for e in x]
+
+
+def make_example_plot2(datadir):
+
+    x, d = get_data(os.path.join(datadir, "attention_weights_gpt2_colon-colon-p1.npz"))
+
+    fig, ax = plt.subplots(3, 1, figsize=(8, 7), sharex="all")
+    
+    t_start=0
+    t_end=46
+
+    xticklabels = replace_nouns([e.strip("Ġ") for e in d["tokens"][0][t_start:t_end]], ["N1", "N2", "N2"])
+
+    layers = [0, 4, 10]
+
+    data = np.mean(x, axis=0) # average across sequences
+
+    im1 = ax[0].imshow(data[:, :, layers[0]][t_start:t_end, ...].T, vmin=0, vmax=1, cmap=plt.cm.Blues)
+    im2 = ax[1].imshow(data[:, :, layers[1]][t_start:t_end, ...].T, vmin=0, vmax=1, cmap=plt.cm.Blues)
+    im3 = ax[2].imshow(data[:, :, layers[2]][t_start:t_end, ...].T, vmin=0, vmax=1, cmap=plt.cm.Blues)
+    
+    cax1 = ax[0].inset_axes([1.02, 0, 0.02, 1])
+    cax2 = ax[1].inset_axes([1.02, 0, 0.02, 1])
+    cax3 = ax[2].inset_axes([1.02, 0, 0.02, 1])
+    
+    cbar1 = plt.colorbar(im1, cax=cax1)
+    cbar2 = plt.colorbar(im2, cax=cax2)
+    cbar3 = plt.colorbar(im3, cax=cax3)
+
+    lfs = 12
+
+    cbar1.ax.set_ylabel("Avg. attention weight")
+    cbar2.ax.set_ylabel("Avg. attention weight")
+    cbar3.ax.set_ylabel("Avg. attention weight")
+
+    ax[2].set_xlabel("Token", fontsize=lfs)
+    ax[2].set_xticks(np.arange(len(xticklabels)))
+    ax[2].set_xticklabels(xticklabels, rotation=90, fontsize=10)
+
+    for i, a in enumerate(ax):
+        a.set_title(f"Layer {layers[i]+1}")
+
+    for a in ax:
+        a.set_yticks(np.arange(0, 12, 1))
+        a.set_yticklabels(np.arange(0, 12, 1)+1)
+
+    fig.supylabel("Attention head", fontsize=lfs)
+    plt.suptitle("Average attention weights across sequences (N = 230)")
+
+    for a in ax:
+        a.grid(visible=True, linewidth=0.5)
+    plt.tight_layout()
+
+    return fig
 
 
 def generate_plot(datadir, layer, sequence):
@@ -142,11 +206,16 @@ def main(input_args=None):
 
         lay, seq = 10, 5
         fig = generate_plot(datadir=args.datadir, layer=lay, sequence=seq)
-        plt.show()
 
         if args.savedir:
             save_png_pdf(fig, savename=os.path.join(args.savedir, f"attn_weights_example_{lay}-{seq}"))
 
+        fig = make_example_plot2(args.datadir)
+
+        if args.savedir:
+            save_png_pdf(fig, savename=os.path.join(args.savedir, f"attn_weights_avg_layer-1-5-11"))
+
+        plt.show()
 
     return 0
 
