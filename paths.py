b5eedf646ec508cf7330bfdf4ec44ec02d678ba0
KristijanArmeni <kristijan.armeni@gmail.com>
2023-07-21 10:15:24 -0400

    - add docs to paths.py - add empty pathsconfig.toml

--- paths.py
+++ paths.py
@@ -1,3 +1,27 @@
+"""
+paths.py is a small helper function can be used to set project paths at the start of a python session.
+It reads contents from pathsconfig.toml file. pathsconfig.toml should be placed in the same directory as paths.py (root)
+
+Examples
+--------
+pathsconfig.toml can look like:
+```toml
+[paths]
+src = /path/to/project/root/src
+```
+
+```
+# assuming ./pathsconfig.toml exists
+python paths.py   # it will add /path/to/project/root/src to sys.path
+```
+
+Or it can be used as a module:
+```
+from paths import PATHS
+
+PATHS.src  # evaluates to /path/to/project/root/src
+```
+"""
 import configparser
 import logging
 import sys
