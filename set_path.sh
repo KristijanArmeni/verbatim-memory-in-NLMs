3c6511853f4f30a4b4125f064221f78eb98a0ff3
KristijanArmeni <kristijan.armeni@gmail.com>
2023-07-21 10:03:46 -0400

    - add requirements.txt and enviroment.yml - remove old environment files - rename proj_paths.py

--- set_path.sh
+++
@@ -1,9 +0,0 @@
-source ./config
-
-echo "Current PROJ_ROOT env variable set to: "$PROJ_ROOT
-echo "(If this is not good, modify PROJ_ROOT variable in the ./config file"
-echo "and call this command again)"
-export PROJ_ROOT=$PROJ_ROOT
-
-echo "Adding it to PYTHONPATH"
-export PYTHONPATH=$PYTHONPATH:$PROJ_ROOT
