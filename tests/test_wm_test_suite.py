56de3e05d4ebae8d972b5444990bf4a7cb2c8b23
KristijanArmeni <kristijan.armeni@gmail.com>
2023-07-28 12:59:10 -0400

    - update to use the new paths.py module

--- tests/test_wm_test_suite.py
+++ tests/test_wm_test_suite.py
@@ -1,10 +1,11 @@
 import os
 from src.wm_suite import wm_test_suite
+from src.wm_suite.utils import get_paths
+from paths import PATHS as p
 
 def test_wm_test_suite():
-
 
     wm_test_suite.main(["--test_run", 
                         "--device", "cuda",
                         "--output_filename", "test_run.csv", 
-                        "--output_dir", os.path.join(os.environ['PROJ_ROOT'], "src", "tests", "data")])
+                        "--output_dir", os.path.join(p.root, "src", "tests", "data")])
