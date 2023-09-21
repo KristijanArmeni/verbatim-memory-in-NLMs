786abab1b0f638c031e0c2c323debfaeb0ffb74a
KristijanArmeni <kristijan.armeni@gmail.com>
2023-09-21 17:23:16 -0400

    - update: move towards using .yaml file to read input data

--- src/wm_suite/viz/ablation/inputs.py
+++ src/wm_suite/viz/ablation/inputs.py
@@ -2,6 +2,7 @@
 import configparser
 from typing import Dict
 import logging
+import yaml
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(funcname)s() | %(message)s")
 
@@ -9,9 +10,14 @@ def get_filenames(pyfilename: str) -> Dict:
 
     # read filenames from pathsconfig.toml
     cfg = configparser.ConfigParser()
-    logging.info(f"Reading entries in datapaths.toml for {pyfilename}")
-    cfg.read("datapaths.toml")
+    logging.info(f"Reading entries in inputs.yaml for {pyfilename}")
+    #cfg.read("datapaths.toml")
 
-    fnames = {**cfg[pyfilename]}
+    with open("inputs.yaml", "r") as f:
+        cfg = yaml.safe_load(f)
+
+    fnames = cfg[pyfilename]
+    print_names = "".join([f'- {k}: {v}\n' for k, v in fnames.items()])
+    logging.info(f"Specified module input files:\n{print_names}")
 
     return fnames
