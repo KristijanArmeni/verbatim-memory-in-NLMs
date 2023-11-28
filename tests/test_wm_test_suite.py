d06fd801054a320b27da89feea5b5bf344d8a542
KristijanArmeni <kristijan.armeni@gmail.com>
2023-11-28 16:59:31 -0500

    [ENH] test wm_test_suite on two checkpoints

--- tests/test_wm_test_suite.py
+++ tests/test_wm_test_suite.py
@@ -5,7 +5,17 @@ p = get_paths()
 
 def test_wm_test_suite():
 
+    # test gpt2
     wm_test_suite.main(["--test_run", 
-                        "--device", "cuda",
-                        "--output_filename", "test_run.csv", 
-                        "--output_dir", "./data"])
+                        "--tokenizer", "gpt2", 
+                        "--checkpoint", "gpt2", 
+                        "--aggregate_output", 
+                        "--aggregate_positions", "[0, 1, 2]"])
+    
+    # test a different checkpoint
+    wm_test_suite.main(["--test_run", 
+                        "--tokenizer", "EleutherAI/pythia-14m", 
+                        "--checkpoint", "EleutherAI/pythia-14m", 
+                        "--aggregate_output", 
+                        "--aggregate_positions", "[0, 1, 2]"])
+    
