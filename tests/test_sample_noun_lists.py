24a2afa5e2852882fab858a698ce785f72611717
KristijanArmeni <kristijan.armeni@gmail.com>
2024-01-24 15:56:56 -0500

    [MAINT] ruff

--- tests/test_sample_noun_lists.py
+++ tests/test_sample_noun_lists.py
@@ -3,23 +3,23 @@ from wm_suite.io.sample_noun_lists import chunk_lists_and_circular_shift
 
 
 def test_chunk_lists_and_circular_shift():
-
     # set up a small list of nouns
-    nouns = ["whisky",
-             "water",
-             "vase",
-             "umbrella",
-             "turtle",
-             "tree",
-             "tractor",
-             "toothbrush",
-             "tomato",
-             "toad",
-             "tiger",
-             "telescope",
-             "tablespoon",
-             "spaghetti",
-             "saxophone",
+    nouns = [
+        "whisky",
+        "water",
+        "vase",
+        "umbrella",
+        "turtle",
+        "tree",
+        "tractor",
+        "toothbrush",
+        "tomato",
+        "toad",
+        "tiger",
+        "telescope",
+        "tablespoon",
+        "spaghetti",
+        "saxophone",
     ]
 
     out = chunk_lists_and_circular_shift(nouns, list_length=3)
@@ -49,10 +49,31 @@ def test_chunk_lists_and_circular_shift():
     assert len(out["n7"]) == n_total_lists
 
     # test that each noun occurs in all positions test the first chunk
-    assert np.array_equal(out["n7"][0], ["whisky", "water", "vase", "umbrella", "turtle", "tree", "tractor"])
-    assert np.array_equal(out["n7"][1], ["tractor", "whisky", "water", "vase", "umbrella", "turtle", "tree"])
-    assert np.array_equal(out["n7"][2], ["tree", "tractor", "whisky", "water", "vase", "umbrella", "turtle"])
-    assert np.array_equal(out["n7"][3], ["turtle", "tree", "tractor", "whisky", "water", "vase", "umbrella"])
-    assert np.array_equal(out["n7"][4], ["umbrella", "turtle", "tree", "tractor", "whisky", "water", "vase"])
-    assert np.array_equal(out["n7"][5], ["vase", "umbrella", "turtle", "tree", "tractor", "whisky", "water"])
-    assert np.array_equal(out["n7"][6], ["water", "vase", "umbrella", "turtle", "tree", "tractor", "whisky"])
+    assert np.array_equal(
+        out["n7"][0],
+        ["whisky", "water", "vase", "umbrella", "turtle", "tree", "tractor"],
+    )
+    assert np.array_equal(
+        out["n7"][1],
+        ["tractor", "whisky", "water", "vase", "umbrella", "turtle", "tree"],
+    )
+    assert np.array_equal(
+        out["n7"][2],
+        ["tree", "tractor", "whisky", "water", "vase", "umbrella", "turtle"],
+    )
+    assert np.array_equal(
+        out["n7"][3],
+        ["turtle", "tree", "tractor", "whisky", "water", "vase", "umbrella"],
+    )
+    assert np.array_equal(
+        out["n7"][4],
+        ["umbrella", "turtle", "tree", "tractor", "whisky", "water", "vase"],
+    )
+    assert np.array_equal(
+        out["n7"][5],
+        ["vase", "umbrella", "turtle", "tree", "tractor", "whisky", "water"],
+    )
+    assert np.array_equal(
+        out["n7"][6],
+        ["water", "vase", "umbrella", "turtle", "tree", "tractor", "whisky"],
+    )
